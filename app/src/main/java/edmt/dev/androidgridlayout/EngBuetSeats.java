a2b7a104f5c31674a51307a00aaa72e05b6f9389
Shahriar <shahriariqbal@gmail.com>
2018-10-22 00:54:05 +0600

    eng_webview

--- app/src/main/java/edmt/dev/androidgridlayout/EngBuetSeats.java
+++ app/src/main/java/edmt/dev/androidgridlayout/EngBuetSeats.java
@@ -2,12 +2,37 @@ package edmt.dev.androidgridlayout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class EngBuetSeats extends AppCompatActivity {
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eng_buet_seats);
+
+
+        webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://drive.google.com/file/d/1X8JG_Lxp0b4qZDX6w0P3__yJW8Nd7Gho/view?fbclid=IwAR1J2MuKTJ8574SC0oHx0e6qMOc8pJkHgjHk4fFb3pxjjlsW7jtkOwYaQJg");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+        super.onBackPressed();
     }
 }
--- app/src/main/java/edmt/dev/androidgridlayout/EngMistOptions.java
+++ app/src/main/java/edmt/dev/androidgridlayout/EngMistOptions.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 
 public class EngMistOptions extends AppCompatActivity {
 
-    private Button MistNotice, MistSeats;
+    private Button MistNotice, MistResult, MistDepartment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +16,8 @@ public class EngMistOptions extends AppCompatActivity {
         setContentView(R.layout.activity_eng_mist_options);
 
         MistNotice = (Button) findViewById(R.id.MistNotice);
+        MistResult = (Button) findViewById(R.id.MistResult) ;
+        MistDepartment = (Button) findViewById(R.id.MistDepartment);
 
 
         MistNotice.setOnClickListener(new View.OnClickListener() {
@@ -24,10 +26,35 @@ public class EngMistOptions extends AppCompatActivity {
                 openMistNotice();
             }
         });
+
+        MistResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMistResult();
+            }
+        });
+
+        MistDepartment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMistDepartment();
+            }
+        });
     }
 
     public void openMistNotice() {
         Intent intent = new Intent(this, EngMistNotice.class);
         startActivity(intent);
     }
+
+    public void openMistResult(){
+        Intent intent = new Intent(this, EngMistResult.class);
+        startActivity(intent);
+    }
+
+    public void openMistDepartment() {
+        Intent intent = new Intent(this, EngMistDepartment.class);
+        startActivity(intent);
+    }
+
 }
